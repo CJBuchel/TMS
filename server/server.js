2d4ae72313ffc703663d8625cb2bdff5bd0d8254
Connor <connorbuchel@gmail.com>
2021-11-18 00:54:22 +0800

    Added routes

--- server/server.js
+++ server/server.js
@@ -1,16 +1,51 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 
-const db = mysql.createPool({
+const user_table = "users";
+const fll_teams_table = "fll_teams";
+
+const db = mysql.createConnection({
 	host: 'localhost',
 	user: 'fss',
 	password: 'fss',
-	database: 'fss_fll_database'
+	database: 'fss_fll_database',
+	insecureAuth: true
 });
 
-app.get('/', (req, res) => {
-	res.send("Hello world");
+db.connect(function(err) {
+	if (err) {
+		console.log("Database connection error");
+		throw err;
+	}
+	console.log("Connected!");
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/api/updateUser', (req, res) => {
+	const user = req.body.user;
+	const value = req.body.password;
+	const sql = "UPDATE users SET password = '" + value + "' WHERE name = '" + user + "';";
+
+
+	db.query(sql, (err, result) => {
+		console.log("User: " + user);
+		console.log("Pass: " + value);
+		if (err) {
+			console.log("User Update error");
+			throw err;
+		} else {
+			console.log(sql);
+			console.log(result);
+		}
+	});
 });
 
 app.listen(3001, () => {

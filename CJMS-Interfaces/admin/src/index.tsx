3513b2ff5f5bab324973f4761c5befc3d77267ad
CJBuchel <connorbuchel@gmail.com>
2022-03-08 10:37:41 +0800

    Testing connection to host server

--- CJMS-Interfaces/admin/src/index.tsx
+++ CJMS-Interfaces/admin/src/index.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React, {useState, useEffect} from 'react';
+
+const NoInternetConnection = (props:any) => {
+  // state variable holds the state of the internet connection
+  const [isOnline, setOnline] = useState(true);
+
+  var count = 0;
+  useEffect(() => {
+    setOnline(navigator.onLine);
+
+    setInterval(() => {
+      count++;
+      fetch("http://"+window.location.host)
+      .then(res => {
+        setOnline(true);
+      }).catch((error:any) => {
+        setOnline(false);
+      });
+    }, 2000);
+
+  },[]);
+
+  // if user is online, return the child component else return a custom component
+  if (isOnline) { 
+    return (
+      props.children
+    )
+  } else {
+    return (
+      <h1>No Internet Connection. Please try again later.</h1>
+    )
+  }
+}
 
 function App() {
+  console.log(window.location.host);
   return (
-    <div>Test</div>
+    <div>
+      <NoInternetConnection>Connection</NoInternetConnection>
+    </div>
   );
 }
 

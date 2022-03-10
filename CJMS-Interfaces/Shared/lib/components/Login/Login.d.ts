4da9f668d92aac443a48fe7aa55948d0692463e5
CJBuchel <connorbuchel@gmail.com>
2022-03-11 07:36:05 +0800

    Added Login Base (untested):

--- CJMS-Interfaces/Shared/lib/components/Login/Login.d.ts
+++ CJMS-Interfaces/Shared/lib/components/Login/Login.d.ts
@@ -1,1 +1,18 @@
+import React from "react";
+interface IProps {
+    setToken: any;
+    allowedUser: any;
+}
+interface IState {
+    username: String;
+    password: String;
+    userOptions: any;
+}
+export declare class Login extends React.Component<IProps, IState> {
+    constructor(props: any);
+    loginUser(credentials: any, allowedUser: any): Promise<false | RequestInfo>;
+    handleSubmit: (e: any) => Promise<void>;
+    onUserChange: (user: string) => void;
+    render(): JSX.Element;
+}
 export {};

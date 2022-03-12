de943566aad1a96ffeb1c6ca293bee16ab26beea
CJBuchel <connorbuchel@gmail.com>
2022-03-12 15:05:07 +0800

    Added mhub (breaking)

--- CJMS-Interfaces/Shared/lib/index.js
+++ CJMS-Interfaces/Shared/lib/index.js
@@ -2,4 +2,5 @@ import { CJMS_Application } from "./components/Application/Application";
 import useToken from "./components/Login/UseToken";
 import Login from "./components/Login/Login";
 import * as request_namesapce from "./components/Requests/Namespaces";
-export { CJMS_Application, useToken, Login, request_namesapce };
+import * as commService from "./components/CommService";
+export { CJMS_Application, useToken, Login, request_namesapce, commService };

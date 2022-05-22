b33f63aeec1fc15117eb717a472d2b308b5372ec
CJBuchel <connorbuchel@gmail.com>
2022-05-23 02:40:41 +0800

    Added router

---
+++ CJMS-Interfaces/MatchControl/src/index.tsx
@@ -0,0 +1,39 @@
+import ReactDOM from 'react-dom';
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {Login, CJMS_Application, useToken, comm_service } from '@cjms_interfaces/shared';
+
+
+
+function IndexRouter() {
+  return (
+    <Router>
+      <Routes>
+        {/* Match Control Page Redirect (Landing) */}
+        <Route path="/" element={<Navigate to="/MatchControl"/>}/>
+
+        {/* Match Control Page */}
+        <Route path="/MatchControl" element={null}/>
+
+        <Route path="/MatchStats" element={null}/>
+      </Routes>
+    </Router>
+  );
+}
+
+function App() {
+  const { token, setToken } = useToken();
+  if (!token) {
+    console.log("Token not made, redirecting...");
+    return <Login setToken={setToken} allowedUsers={['admin', 'head_referee']}/>;
+  } else {
+    console.log("Token made");
+    return (
+      <div>
+        <IndexRouter/>
+      </div>
+    );
+  }
+}
+
+CJMS_Application(App);

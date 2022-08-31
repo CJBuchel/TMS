d76c070028305788eb8b911aaa7df726add63144
CJBuchel <connorbuchel@gmail.com>
2022-09-01 04:40:58 +0800

    CSV upload (unprocessed)

--- CJMS-Interfaces/Admin/src/components/Setup/Setup.tsx
+++ CJMS-Interfaces/Admin/src/components/Setup/Setup.tsx
@@ -1,27 +1,60 @@
-import React, {Component} from "react";
+import React, {ChangeEvent, Component, InputHTMLAttributes, useState} from "react";
+import Papa from "papaparse";
 
 import "../../assets/Setup.scss"
+import { callbackify } from "util";
 
 interface IProps {}
-interface IState {}
+interface IState {
+  csv:any;
+}
 
 export default class Setup extends Component<IProps, IState> {
   constructor(props:any) {
     super(props);
-    this.state = {}
+    this.state = {
+      csv:null
+    }
+
+    this.updateCSV = this.updateCSV.bind(this);
+  }
+
+  updateCSV(result:any) {
+    this.setState({csv:result.data});
+    console.log(result.data);
+  }
+
+  onScheduleImport(e:ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    const f:any = (e.target as HTMLInputElement).files?.[0];
+    Papa.parse(f, {header:false, skipEmptyLines:true, complete:this.updateCSV});
+  }
+
+  handleSubmit() {
+    
   }
 
   render() {
     return(
       <div className="setup-main">
         <div className="container">
-          <h3>Schedule</h3>
-          <a href="">Upload CSV</a><br/>
-          <a href="https://firstaustralia.org/fll-scheduler/">Generate Schedule/CSV</a><br/>
+
+          {/* Main Form */}
+          <form>
+            <h1>Schedule</h1>
+            <input 
+              type={"file"} 
+              id={"scheduleImport"}
+              accept={".csv"}
+              onChange={(e:ChangeEvent<HTMLInputElement>) => this.onScheduleImport(e)}
+            />
+            or - <a href="https://firstaustralia.org/fll-scheduler/">Generate Schedule/CSV</a><br/>
 
 
-          <h3>Event Name</h3>
-          <input/>
+            <h1>Event Name</h1>
+            <input placeholder="event name..."/>
+          </form>
+
         </div>
       </div>
     );

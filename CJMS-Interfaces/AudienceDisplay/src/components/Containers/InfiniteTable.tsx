0ef3b12e7cd8492fd37dbd45ff458faa1f74265f
CJBuchel <connorbuchel@gmail.com>
2022-09-05 00:44:16 +0800

    Added working inf scroll table

--- CJMS-Interfaces/AudienceDisplay/src/components/Containers/InfiniteTable.tsx
+++ CJMS-Interfaces/AudienceDisplay/src/components/Containers/InfiniteTable.tsx
@@ -9,6 +9,7 @@ interface IProps {}
 
 interface IState {
   scrollTop:number;
+  table_body:any;
 }
 
 export default class InfiniteTable extends Component<IProps, IState> {
@@ -16,7 +17,8 @@ export default class InfiniteTable extends Component<IProps, IState> {
     super(props);
 
     this.state = {
-      scrollTop: 0
+      scrollTop: 0,
+      table_body: React.createRef()
     }
 
     this.scrollCallback = this.scrollCallback.bind(this);
@@ -25,44 +27,40 @@ export default class InfiniteTable extends Component<IProps, IState> {
   unmountScroll = false;
 
   scrollCallback() {
-    if (this.unmountScroll) {
-      return;
-    } else {
+    if (!this.unmountScroll) {
       this.setState({scrollTop: this.newScroll(this.state.scrollTop)});
       window.requestAnimationFrame(() => this.scrollCallback());
     }
   }
 
   componentDidMount() {
+    this.unmountScroll = false;
     setTimeout(() => window.requestAnimationFrame(() => this.scrollCallback()), DEFAULT_DELAY);
   }
 
   componentWillUnmount() {
     this.unmountScroll = true;
-    // this.setState({unmountScroll:true});
-    // window.location.reload();
-    // clearInterval();
   }
 
   bodyScrollHeight() {
-    return this.state.scrollTop + (document.getElementById('table-body')?.scrollHeight || 0); // document.body.scrollHeight
+    return this.state.scrollTop + this.state.table_body.current?.scrollHeight || 0; // document.body.scrollHeight
   }
   
   isScrolling() {
-    // console.log(document.getElementById('table-body')?.clientHeight);
-
-    return ((document.getElementById('table-body')?.clientHeight || 0) < this.bodyScrollHeight());
+    return window.innerHeight < this.bodyScrollHeight() ? true : false;
   }
 
   newScroll(oldScroll:number) {
     const height = this.bodyScrollHeight();
+    // console.log("Height: " + height);
+    // console.log("OldScroll: " + oldScroll);
     if (!this.isScrolling()) {
-      // console.log("not scrolling");
       return 0;
-    } else if (oldScroll >= height/2) {
+    } else if (oldScroll >= height/3) {
       console.log("Resetting");
-      return oldScroll - height/2;
+      return (oldScroll - height/3);
     } else {
+      // console.log("scrolling");
       return oldScroll + (DEFAULT_SPEED/2);
     }
   }
@@ -86,21 +84,37 @@ export default class InfiniteTable extends Component<IProps, IState> {
     );
   }
 
+
   renderTable() {
     const firstTable = this.getFormattedData();
     const secondTable = this.getFormattedData();
 
-    return(
-      <tbody id='table-body' style={{overflow: 'hidden'}}>
-        {firstTable}
-        {secondTable}
-      </tbody>
-    );
+    // const scrolling = Object.keys(this.refs).length ? this.isScrolling() : false;
+    // const scrolling = this.isScrolling();
+
+    // if (this.isScrolling())
+    const scrolling = this.isScrolling();
+    // const scrolling = this.isScrolling() == false ? false : true;
+
+    if (scrolling) {
+      return(
+        <tbody id='table-body' ref={this.state.table_body}>
+          {firstTable}
+          {secondTable}
+        </tbody>
+      );
+    } else {
+      return(
+        <tbody id='table-body' ref={this.state.table_body}>
+          {firstTable}
+        </tbody>
+      );
+    }
   }
 
   render() {
     return (
-      <div className='table-wrapper ui segment' style={{direction: 'ltr', marginTop: -this.state.scrollTop}}>
+      <div id='table-wrapper' className='table-wrapper' style={{direction: 'ltr', marginTop: -this.state.scrollTop}}>
         <table id='table-parent' className='fl-table ui scrollable single line very basic compact table'>
           <thead id='table-head'>
             <tr>

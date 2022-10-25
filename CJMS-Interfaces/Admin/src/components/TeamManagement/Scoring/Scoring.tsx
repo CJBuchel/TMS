6155291a178845a8e6884b8cf565730ee9786460
CJBuchel <connorbuchel@gmail.com>
2022-10-25 15:24:03 +0800

    Started work on scorekeeper page

---
+++ CJMS-Interfaces/Admin/src/components/TeamManagement/Scoring/Scoring.tsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+interface IProps {}
+
+interface IState {}
+
+export default class Scoring extends Component<IProps, IState> {
+  constructor(props:any) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render() {
+    return(0);
+  }
+}

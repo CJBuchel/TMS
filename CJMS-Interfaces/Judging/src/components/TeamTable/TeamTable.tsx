aed9bb43ec10e92a88369de4a83ed3dcef940f73
CJBuchel <connorbuchel@gmail.com>
2022-11-10 19:09:48 +0800

    Added colour coding

--- CJMS-Interfaces/Judging/src/components/TeamTable/TeamTable.tsx
+++ CJMS-Interfaces/Judging/src/components/TeamTable/TeamTable.tsx
@@ -1,5 +1,5 @@
 import { IEvent, IMatch, ITeam } from "@cjms_shared/services";
-import { Component } from "react";
+import React, { Component } from "react";
 
 interface IProps {
   external_eventData:IEvent;
@@ -40,20 +40,47 @@ export default class TeamTable extends Component<IProps, IState> {
   }
 
   getTableScores(team:ITeam) {
-    var table_scores:string[] = [];
+    var table_scores:{score:string, gp:string}[] = [];
     for (var i = 0; i < this.props.external_eventData.event_rounds; i++) {
       if (team.scores[i]) {
-        table_scores.push(team.scores[i].score.toString());
-        table_scores.push(team.scores[i].gp.toString());
+        table_scores.push({score:team.scores[i].score.toString(), gp:team.scores[i].gp});
       } else {
-        table_scores.push('');
-        table_scores.push('');
+        table_scores.push({score:'', gp:''});
       }
     }
 
-    return (
-      table_scores.map((score, i) => (<td key={i}>{`${score}`}</td>))
-    );
+    if (this.props.external_matchData.length > 0) {
+      const team_matches = this.props.external_matchData.filter(match => match.on_table1.team_number === team.team_number || match.on_table2.team_number === team.team_number);
+      team_matches.sort((a:any,b:any) => {return a.match_number-b.match_number});
+      console.log(team_matches[1].complete);
+      return (
+        table_scores.map((score,i) => {
+          const onTable = team_matches[i].on_table1.team_number === team.team_number ? team_matches[i].on_table1 : team_matches[i].on_table2;
+          return (
+            <React.Fragment key={i}>
+              <td
+                style={{
+                  backgroundColor : `${
+                    team_matches[i].deferred ? 'cyan' :
+                    team_matches[i].complete && !onTable.score_submitted ? 'red' : 
+                    team_matches[i].complete && onTable.score_submitted ? 'green' : ''
+                  }`
+                }}
+              >{`${score.score}`}</td>
+              <td
+                style={{
+                  backgroundColor : `${
+                    team_matches[i].deferred ? 'cyan' :
+                    team_matches[i].complete && !onTable.score_submitted ? 'red' : 
+                    team_matches[i].complete && onTable.score_submitted ? 'green' : ''
+                  }`
+                }}
+              >{`${score.gp}`}</td>
+            </React.Fragment>
+          )
+        })
+      );
+    }
   }
 
   getTable() {

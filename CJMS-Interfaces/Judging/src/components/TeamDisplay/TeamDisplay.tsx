86502a45835b0a6df730f35b2f1260e5900d5748
CJBuchel <connorbuchel@gmail.com>
2022-11-10 18:08:02 +0800

    Added judge display

---
+++ CJMS-Interfaces/Judging/src/components/TeamDisplay/TeamDisplay.tsx
@@ -0,0 +1,62 @@
+import { IEvent, IMatch, ITeam } from "@cjms_shared/services";
+import { Component } from "react";
+import { TeamTable } from "../TeamTable";
+
+import { Button } from "@mui/material";
+
+import "../../assets/stylesheets/TeamDisplay.scss";
+import "../../assets/stylesheets/TeamTable.scss";
+import Export from "./Export";
+
+interface IProps {
+  external_eventData:IEvent;
+  external_matchData:IMatch[];
+  external_teamData:ITeam[];
+}
+
+interface IState {}
+
+export default class TeamDisplay extends Component<IProps, IState> {
+  constructor(props:any) {
+    super(props);
+
+    this.state = {}
+  }
+
+  handleExport(comments:boolean = false) {
+    const export_teams = new Export(this.props.external_teamData, this.props.external_eventData.event_rounds);
+    export_teams.exportCSV(comments);
+  }
+
+  render() {
+    return(
+      <div className="team-display">
+        <div className="export-buttons">
+          <Button
+            variant="contained"
+            onClick={() => this.handleExport()}
+            sx={{
+              margin: '10px 25px',
+              backgroundColor: 'teal'
+            }}
+          >Export</Button>
+          <Button
+            variant="contained"
+            onClick={() => this.handleExport(true)}
+            sx={{
+              margin: '10px 25px',
+              backgroundColor: 'teal'
+            }}
+          >Export w/Comments</Button>
+        </div>
+        <div className="table-wrapper">
+          <TeamTable
+            external_eventData={this.props.external_eventData}
+            external_teamData={this.props.external_teamData}
+            external_matchData={this.props.external_matchData}
+          />
+        </div>
+      </div>
+    );
+  }
+}

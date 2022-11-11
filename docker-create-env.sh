4800116466272382036d783ae6e97272d82ba806
CJBuchel <connorbuchel@gmail.com>
2022-11-11 17:40:44 +0800

    Bumped patch version 1.2

--- docker-create-env.sh
+++ docker-create-env.sh
@@ -1,7 +1,7 @@
 # 
 # Random password generated for application to use
 # 
-VERSION="2022.1.1";
+VERSION="2022.1.2";
 CJMS_VERSION="DOCKER_TAG=$VERSION";
 REACT_APP_CJMS_VERSION="REACT_APP_CJMS_VERSION=$VERSION"
 

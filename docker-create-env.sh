ac647466e897af45da5b990d081ad81978867d99
CJBuchel <connorbuchel@gmail.com>
2022-11-11 20:44:44 +0800

    patched sounds to be less tremble:

--- docker-create-env.sh
+++ docker-create-env.sh
@@ -1,7 +1,7 @@
 # 
 # Random password generated for application to use
 # 
-VERSION="2022.1.2";
+VERSION="2022.1.3";
 CJMS_VERSION="DOCKER_TAG=$VERSION";
 REACT_APP_CJMS_VERSION="REACT_APP_CJMS_VERSION=$VERSION"
 

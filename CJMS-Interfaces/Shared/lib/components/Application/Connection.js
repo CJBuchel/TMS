a3aae2a649c4c8061087ef74b117cef55ebd0390
CJBuchel <connorbuchel@gmail.com>
2022-11-13 20:49:08 +0800

    Added refresh all interfaces button

--- CJMS-Interfaces/Shared/lib/components/Application/Connection.js
+++ CJMS-Interfaces/Shared/lib/components/Application/Connection.js
@@ -2,42 +2,51 @@
 // Connection to the server (monitor modal)
 // 
 import { comm_service } from "@cjms_shared/services";
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import "../../assets/stylesheets/ConnectionModal.scss";
-var count = 0;
-export const ConnectionCheck = (props) => {
-    const [isOnline, setOnline] = useState(true);
-    const [retryCount, setRetryCount] = useState(0);
-    useEffect(() => {
+export class ConnectionCheck extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOnline: false,
+            retryCount: 0
+        };
         comm_service.listeners.onSystemRefresh(() => {
-            console.log('refreshing');
             window.location.reload();
         });
-        setInterval(() => {
-            fetch("http://" + window.location.host)
-                .then(res => {
-                setOnline(true);
-                count = 0;
-                setRetryCount(0);
-            }).catch((error) => {
-                count++;
-                setRetryCount(count);
-                setOnline(false);
-            });
-        }, 2000);
-    }, []);
-    function AppRender() {
-        // If there is a connection to server render normally
-        if (isOnline) {
-            return (props.children);
+        this.connectionCheck = this.connectionCheck.bind(this);
+    }
+    async componentDidMount() {
+        this.startLoop();
+    }
+    componentWillUnmount() {
+        clearInterval(this.state.loop);
+    }
+    connectionCheck() {
+        fetch("http://" + window.location.host).then(res => {
+            this.setState({ isOnline: true });
+            this.setState({ retryCount: 0 });
+        }).catch((error) => {
+            this.setState({ isOnline: false });
+            var count = this.state.retryCount;
+            this.setState({ retryCount: count++ });
+        });
+    }
+    startLoop() {
+        this.connectionCheck();
+        this.setState({ loop: setInterval(this.connectionCheck, 2000) });
+    }
+    render() {
+        if (this.state.isOnline) {
+            return (this.props.app);
         }
         else {
-            var retryString = retryCount.toString();
-            if (retryCount === 69) {
+            var retryString = this.state.retryCount.toString();
+            if (this.state.retryCount === 69) {
                 retryString = retryString + " Nice";
             }
             return (React.createElement("div", { className: "ConnectionWrapper" },
-                React.createElement("div", { className: "ConnectionModalApp" }, props.children),
+                React.createElement("div", { className: "ConnectionModalApp" }, this.props.app),
                 React.createElement("div", { className: "ConnectionModalBackdrop" }),
                 React.createElement("div", { className: "ConnectionModal animated fadeIn" },
                     React.createElement("h1", null, "NO CONNECTION"),
@@ -47,5 +56,4 @@ export const ConnectionCheck = (props) => {
                         retryString))));
         }
     }
-    return (AppRender());
-};
+}

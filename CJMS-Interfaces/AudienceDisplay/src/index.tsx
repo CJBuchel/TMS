4a683ce6911733e1c867b0643b48c152a554b5e6
CJBuchel <connorbuchel@gmail.com>
2022-11-15 16:45:21 +0800

    Added better audience display (might have performance issues)

--- CJMS-Interfaces/AudienceDisplay/src/index.tsx
+++ CJMS-Interfaces/AudienceDisplay/src/index.tsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 import { CJMS_Application } from '@cjms_interfaces/shared';
 // import { Setup } from './components/Setup';
 
-import "./assets/application.scss";
+import "./assets/stylesheets/application.scss";
 import { Display } from './components/Display';
 
 function App() {

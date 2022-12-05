6cf8ba988d94a4e5c6c0486843a54565f9953a88
CJBuchel <connorbuchel@gmail.com>
2022-12-06 01:29:03 +0800

    Added next highest score for ranking view

--- CJMS-Servers/Server/src/api/requests/Teams/index.tsx
+++ CJMS-Servers/Server/src/api/requests/Teams/index.tsx
@@ -9,6 +9,26 @@ export class Teams {
   constructor(requestServer:RequestServer) {
     console.log("Team Requests Constructed");
 
+    // returns 0 for first array, 1 for second. -1 for complete tie
+    function getNextHighest(arr1:number[], arr2:number[]): number {
+      var highest = -1;
+
+      while (arr1.length > 0 && arr2.length > 0) {  
+        if (Math.max(...arr1) > Math.max(...arr2)) {
+          highest = 0;
+          break;
+        } else if (Math.max(...arr1) < Math.max(...arr2)) {
+          highest = 1;
+          break;
+        } else {
+          arr1.splice(arr1.indexOf(Math.max(...arr1)), 1);
+          arr2.splice(arr2.indexOf(Math.max(...arr2)), 1);
+        }
+      }
+
+      return highest;
+    }
+
     function updateRankings() {
       const query = TeamModel.find({});
       query.exec(function(err, result) {
@@ -19,8 +39,42 @@ export class Teams {
           for (var i = 0; i < teams.length; i++) {
             var ranking = 1;
             for (var j = 0; j < teams.length; j++) {
-              if ( Math.max(...teams[j].scores.map((score:ITeamScore) => score.score)) > Math.max(...teams[i].scores.map((score:ITeamScore) => score.score))) {
+              if (Math.max(...teams[j].scores.map((score:ITeamScore) => score.score)) > Math.max(...teams[i].scores.map((score:ITeamScore) => score.score))) {
                 ranking++;
+              } else if (Math.max(...teams[j].scores.map((score:ITeamScore) => score.score)) === Math.max(...teams[i].scores.map((score:ITeamScore) => score.score))) {
+                if (teams[j].scores.length > 1 && teams[i].scores.length > 1) {
+                  if (getNextHighest(teams[j].scores.map((score:ITeamScore) => score.score), teams[i].scores.map((score:ITeamScore) => score.score)) === 0) {
+                    ranking++;
+                  }
+                }
+
+                // var next_j = 0, next_i = 0;
+
+                // // get next best for j team
+                // for (var k = 0; k < teams[j].scores.length; k++) {
+                //   // next_j = -1;
+                //   for (var l = k + 1; l < teams[j].scores.length; l++) {
+                //     if (teams[j].scores[k].score < teams[j].scores[l].score) {
+                //       next_j = teams[j].scores[l].score;
+                //       break;
+                //     }
+                //   }
+                // }
+
+                // // get next best for i team
+                // for (var k = 0; k < teams[i].scores.length; k++) {
+                //   // next_i = -1;
+                //   for (var l = k + 1; l < teams[i].scores.length; l++) {
+                //     if (teams[i].scores[k].score < teams[i].scores[l].score) {
+                //       next_i = teams[i].scores[l].score;
+                //       break;
+                //     }
+                //   }
+                // }
+
+
+                // console.log(next_j);
+                // console.log(next_i);
               }
             }
     

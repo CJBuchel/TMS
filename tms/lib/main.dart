ae66792b5c782abf647627070caabcd996509511
CJBuchel <connorbuchel@gmail.com>
2023-04-01 01:34:15 +0800

    Building dart bridge

--- tms/lib/main.dart
+++ tms/lib/main.dart
@@ -1,4 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:tms/pages/landing.dart';
 
-void main() => runApp(const MaterialApp(home: Landing()));
+class TmsHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+void main() {
+  HttpOverrides.global = TmsHttpOverrides();
+  runApp(MaterialApp(home: Landing()));
+}
--- tms/lib/pages/landing.dart
+++ tms/lib/pages/landing.dart
@@ -1,47 +1,34 @@
-import 'dart:async';
-import 'dart:ffi';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:network_info_plus/network_info_plus.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class Landing extends StatefulWidget {
-  const Landing({super.key});
+  Landing({super.key});
 
   @override
   State<Landing> createState() => _LandingState();
 }
 
 class _LandingState extends State<Landing> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final channel = WebSocketChannel.connect(Uri.parse("wss://localhost:2121/echo"));
+
+    channel.stream.listen((data) {
+      print(data);
+    }, onError: (error) {
+      print(error);
+    });
+  }
+
   final info = NetworkInfo();
-  // Future<http.Response> fetchEcho() {
-  //   return http.get(Uri.parse('http://127.0.0.1:2121/'));
-  // }
-
-  // void serverFind() {
-  //   info.getWifiIP().then((value) async {
-  //     final ipArr = value?.split('.');
-
-  //     var client = http.Client();
-  //     for (var i = 1; i < 255; i++) {
-  //       String bufferIP =
-  //           'http://${ipArr?[0]}.${ipArr?[1]}.${ipArr?[2]}.$i:2121/';
-
-  //       try {
-  //         var response = await client.get(Uri.parse(bufferIP));
-  //         print(response);
-  //       } catch (e) {
-  //         print(e);
-  //       }
-  //     }
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
-    // fetchEcho();
-    // serverFind();
     return const Scaffold(
       body: Text("Landing Page"),
     );

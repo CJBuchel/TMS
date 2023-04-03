fb6cb1e6ecb68da6502af76a7d54e2b0ea61663f
CJBuchel <connorbuchel@gmail.com>
2023-04-03 17:14:15 +0800

    Testing github release task

--- server/build/env_generator.rs
+++ server/build/env_generator.rs
@@ -1,4 +1,3 @@
-use std::borrow::Borrow;
 use std::ffi::OsString;
 use std::fs::{self, File};
 use std::io::{BufWriter, Write};
@@ -16,12 +15,13 @@ struct YamlVariables {
   variables: YamlEnv
 }
 
+const VERSION: &str = "2023.0.0";
+
 pub fn generate_env(outdir: &OsString) {
-  let version = "2023.0.0";
 
   let env: Vec<String> = [
-    format!("TMS_TAG={}\n", version),
-    format!("DOCKER_TAG={}\n", version)
+    format!("TMS_TAG={}\n", VERSION),
+    format!("DOCKER_TAG={}\n", VERSION)
   ].to_vec();
 
   let env_path = Path::new(outdir).join(".env");
@@ -32,9 +32,11 @@ pub fn generate_env(outdir: &OsString) {
   for line in &env {
     f.write_all(line.as_bytes()).expect("Could not write to file");
   }
+}
 
+pub fn generate_env_yaml(outdir: &OsString) {
   // Yaml Env
-  let yaml:YamlVariables = YamlVariables { variables: YamlEnv { version: version.to_string() } };
+  let yaml:YamlVariables = YamlVariables { variables: YamlEnv { version: VERSION.to_string() } };
   let env_path = Path::new(outdir).join("env.yaml");
   let f = File::create(env_path).expect("Could not write to file");
   serde_yaml::to_writer(f, &yaml).unwrap();

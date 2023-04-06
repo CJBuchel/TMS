582648426b889ea10d0a4609940a03953cf30aca
CJBuchel <connorbuchel@gmail.com>
2023-04-07 00:59:13 +0800

    Solving windows release not working (http requirements)

--- tms/lib/main.dart
+++ tms/lib/main.dart
@@ -3,28 +3,28 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:tms/pages/landing.dart';
 
-import 'package:path/path.dart' show dirname, join, normalize;
+// import 'package:path/path.dart' show dirname, join, normalize;
 
-String _scriptPath() {
-  var script = Platform.script.toString();
-  if (script.startsWith("file://")) {
-    script = script.substring(7);
-  } else {
-    final idx = script.indexOf("file:/");
-    script = script.substring(idx + 5);
-  }
+// String _scriptPath() {
+//   var script = Platform.script.toString();
+//   if (script.startsWith("file://")) {
+//     script = script.substring(7);
+//   } else {
+//     final idx = script.indexOf("file:/");
+//     script = script.substring(idx + 5);
+//   }
 
-  return script;
-}
+//   return script;
+// }
 
 class TmsHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
-  }
+  // @override
+  // HttpClient createHttpClient(SecurityContext? context) {
+  //   return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  // }
 }
 
 void main() {
-  HttpOverrides.global = TmsHttpOverrides();
+  // HttpOverrides.global = TmsHttpOverrides();
   runApp(MaterialApp(home: Landing()));
 }

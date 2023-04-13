6598507e89fbbccc801a8fd4daf36403574913f0
CJBuchel <connorbuchel@gmail.com>
2023-04-14 06:59:09 +0800

    Added decoding channel chunks

--- server/src/network/security.rs
+++ server/src/network/security.rs
@@ -1,6 +1,8 @@
 use openssl::{rsa::{Rsa, Padding}, pkey::Private};
 use base64::{Engine, engine::general_purpose};
 
+const SIZE: usize = 512;
+
 #[derive(Clone)]
 pub struct Security {
   pub public_key: Vec<u8>,
@@ -9,7 +11,8 @@ pub struct Security {
 
 impl Security {
   pub fn new() -> Self {
-    let raw_rsa = Rsa::generate(2048).unwrap();
+    println!("Size in bytes: {}, {}", SIZE, u32::try_from(SIZE).unwrap());
+    let raw_rsa = Rsa::generate(u32::try_from(SIZE).unwrap()).unwrap();
     let raw_public_key = raw_rsa.public_key_to_pem_pkcs1().unwrap();
 
     Self {
@@ -27,10 +30,17 @@ pub async fn encrypt(security: Security, data: String) -> Vec<u8> {
 }
 
 pub async fn decrypt(security: Security, buf: String) -> String {
-  println!("Buffer: {:?}", buf);
   let decoded_buff = general_purpose::STANDARD.decode(buf).unwrap();
-  let mut data: Vec<u8> = vec![0; security.rsa.size() as usize];
-  let _ = security.rsa.private_decrypt(&decoded_buff, &mut data, Padding::PKCS1).unwrap();
-  println!("Data: {:?}", data);
-  return String::from_utf8(data).unwrap();
+  let decoded_channels = decoded_buff.chunks(SIZE/8);
+  let mut decrypted_data:String = "".to_string();
+  println!("Number of chunks: {:?}", decoded_channels.len());
+  for channel_chunk in decoded_channels {
+    let mut data: Vec<u8> = vec![0; security.rsa.size() as usize];
+    let _ = security.rsa.private_decrypt(channel_chunk, &mut data, Padding::PKCS1).unwrap();
+    decrypted_data.push_str(String::from_utf8(data).unwrap().as_str());
+    // println!("Test get data: {}", String::from_utf8(data.clone()).unwrap());
+  }
+  // println!("Buffer: {:?}", decoded_buff);
+  // println!("Data: {:?}", decrypted_data);
+  return decrypted_data
 }

3eb584343e51356a5b036713cfcd897e17e2fba1
CJBuchel <connorbuchel@gmail.com>
2023-04-28 19:46:47 +0800

    Updated android icons

--- tms/lib/main.dart
+++ tms/lib/main.dart
@@ -35,6 +35,8 @@ class _TMSAppState extends State<TMSApp> {
     if (_connectionTimer.isActive) {
       _connectionTimer.cancel();
     }
+
+    NetworkWebSocket.disconnect();
   }
 
   Future<void> checkConnection() async {

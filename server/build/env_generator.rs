3121d64031acf9da8d629c4b2a762ed241bf0989
CJBuchel <connorbuchel@gmail.com>
2023-09-11 14:40:32 +0800

    Whoops, forgot about the build dir

---
+++ server/build/env_generator.rs
@@ -0,0 +1,46 @@
+use std::ffi::OsString;
+use std::fs::{File};
+use std::io::{BufWriter, Write};
+use std::path::Path;
+
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct YamlEnv {
+  name: String,
+  value: String
+}
+
+#[derive(Debug, Serialize)]
+struct YamlVariables {
+  variables: Vec<YamlEnv>
+}
+
+const VERSION: &str = "2023.0.0";
+
+pub fn generate_env(outdir: &OsString) {
+
+  let env: Vec<String> = [
+    format!("TMS_TAG={}\n", VERSION),
+    format!("DOCKER_TAG={}\n", VERSION)
+  ].to_vec();
+
+  let env_path = Path::new(outdir).join(".env");
+  let f = File::create(env_path).expect("Unable to create file");
+  let mut f = BufWriter::new(f);
+
+  for line in &env {
+    f.write_all(line.as_bytes()).expect("Could not write to file");
+  }
+
+  println!("cargo:rustc-env=VERSION={}", VERSION);
+}
+
+pub fn generate_env_yaml(outdir: &OsString) {
+  // Yaml Env
+  let mut yaml:YamlVariables = YamlVariables { variables: Vec::new() };
+  yaml.variables.push(YamlEnv { name: "version".to_string(), value: VERSION.to_string() });
+  let env_path = Path::new(outdir).join("vars.yml");
+  let f = File::create(env_path).expect("Could not write to file");
+  serde_yaml::to_writer(f, &yaml).unwrap();
+}
---
+++ server/build/schema_generator.rs
@@ -0,0 +1,64 @@
+
+use std::ffi::OsString;
+use std::fs;
+use std::path::Path;
+
+use schemars::JsonSchema;
+use tms_utils::{
+
+  // Storage structure imports
+  schemas::*,
+  
+  // Network structure imports
+  network_schemas::*
+};
+
+#[allow(dead_code)]
+#[derive(JsonSchema, Clone)]
+pub struct TmsSchema {
+  //
+  // Primitive storage structures
+  //
+  team: Team,
+  game_match: GameMatch,
+  judging_session: JudgingSession,
+  users: User,
+  event: Event,
+
+  //
+  // Network structures (Basic)
+  //
+  register_request: RegisterRequest,
+  register_response: RegisterResponse,
+  socket_event: SocketMessage,
+  integrity_message: IntegrityMessage,
+  login_request: LoginRequest,
+  login_response: LoginResponse,
+  start_timer_request: StartTimerRequest,
+  setup_request: SetupRequest,
+  purge_request: PurgeRequest,
+
+  //
+  // Network structures (Event Data)
+  //
+  events_get_response: EventResponse,
+  //teams
+  teams_get_response: TeamsResponse,
+  team_get_request: TeamRequest,
+  team_get_response: TeamResponse,
+  // matches
+  matches_get_response: MatchesResponse,
+  match_get_request: MatchRequest,
+  match_get_response: MatchResponse,
+  //judging
+  judging_sessions_get_response: JudgingSessionsResponse,
+  judging_session_get_request: JudgingSessionRequest,
+  judging_session_get_response: JudgingSessionResponse,
+}
+
+pub fn generate_schema(outdir: &OsString) {
+  let schema = schemars::schema_for!(TmsSchema);
+  let schema_file = Path::new(outdir).join("tms_schema.json");
+
+  fs::write(schema_file, serde_json::to_string_pretty(&schema).unwrap()).unwrap();
+}

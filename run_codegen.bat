e9e83a2aa057d3795d2fe60f56966b93ecf16a5e
CJBuchel <connorbuchel@gmail.com>
2024-10-28 13:11:47 +0800

    switched to cleaner codegen using yaml config

--- run_codegen.bat
+++ run_codegen.bat
@@ -14,11 +14,12 @@ set "TMS_FLUTTER_ENTRY_NAME=TmsRustLib"
 
 REM codegen for flutter (green text)
 echo Running codegen for %TMS_INFRA%
-flutter_rust_bridge_codegen generate --dart-entrypoint-class-name %TMS_FLUTTER_ENTRY_NAME% --rust-input %TMS_CRATES% --rust-root %TMS_INFRA% --dart-output %TMS_FLUTTER%\lib\generated
+cd %TMS_INFRA%
+flutter_rust_bridge_codegen generate
+cd ..
 
 REM build wasm logic
 echo Building wasm for %TMS_INFRA%
-wasm-pack build %TMS_INFRA% --target no-modules --out-dir %TMS_FLUTTER%\web\pkg --no-typescript --out-name tms_infra --dev -- -Z build-std=std,panic_abort
 cd %TMS_FLUTTER%
 dart run flutter_rust_bridge build-web --dart-root %TMS_FLUTTER% --rust-root %TMS_INFRA%
 cd ..

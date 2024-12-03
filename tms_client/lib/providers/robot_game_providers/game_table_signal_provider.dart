2dcaa0d6f6b6d9e24a14c6d6667d9c2668283205
Connor J Buchel <connorbuchel@gmail.com>
2024-12-03 19:14:11 +0800

    Game Table Signal Patch (#196)
    * Made match controller 'clear' game table signals on load/unload
    
    * There is no 'first onload', switching to only unload clear signals
    
    * Added on first load/unload reset signals (using match number equality)
    
    * bumped version

--- tms_client/lib/providers/robot_game_providers/game_table_signal_provider.dart
+++ tms_client/lib/providers/robot_game_providers/game_table_signal_provider.dart
@@ -1,12 +1,13 @@
-import 'package:echo_tree_flutter/widgets/echo_tree_provider.dart';
-import 'package:tms/generated/infra/database_schemas/game_table.dart';
+import 'package:flutter/foundation.dart';
+import 'package:tms/generated/infra/network_schemas/socket_protocol/match_state_event.dart';
 import 'package:tms/generated/infra/network_schemas/socket_protocol/server_socket_protocol.dart';
 import 'package:tms/generated/infra/network_schemas/socket_protocol/table_state_event.dart';
 import 'package:tms/mixins/server_event_subscriber_mixin.dart';
 import 'package:tms/utils/logger.dart';
 
-class GameTableSignalProvider extends EchoTreeProvider<String, GameTable> with ServerEventSubscribeNotifierMixin {
+class GameTableSignalProvider extends ChangeNotifier with ServerEventSubscribeNotifierMixin {
   final Map<String, String> _tableSignals = {};
+  List<String> _loadedMatchNumbers = [];
 
   void _updateTableSignals(String table, String teamNumber) {
     // update or add table signal
@@ -14,11 +15,7 @@ class GameTableSignalProvider extends EchoTreeProvider<String, GameTable> with S
     notifyListeners();
   }
 
-  GameTableSignalProvider()
-      : super(
-          tree: ":robot_game:tables",
-          fromJsonString: (json) => GameTable.fromJsonString(json: json),
-        ) {
+  GameTableSignalProvider() {
     subscribeToEvent(TmsServerSocketEvent.tableStateEvent, (event) {
       if (event.message != null) {
         // handle table state event
@@ -27,6 +24,28 @@ class GameTableSignalProvider extends EchoTreeProvider<String, GameTable> with S
           _updateTableSignals(tableStateEvent.table, tableStateEvent.teamNumber);
         } catch (e) {
           TmsLogger().e("Error parsing table state event: $e");
+        }
+      }
+    });
+    subscribeToEvent(TmsServerSocketEvent.matchStateEvent, (event) {
+      if (event.message != null) {
+        // handle match state event
+        try {
+          TmsServerMatchStateEvent matchStateEvent = TmsServerMatchStateEvent.fromJsonString(json: event.message!);
+          bool load = matchStateEvent.state == TmsServerMatchState.load;
+          bool unload = matchStateEvent.state == TmsServerMatchState.unload;
+
+          // clear table signals on first load or unload
+          if (load || unload) {
+            // check if it's a new match (using match number equality)
+            if (!listEquals(matchStateEvent.gameMatchNumbers, _loadedMatchNumbers)) {
+              _loadedMatchNumbers = matchStateEvent.gameMatchNumbers;
+              _tableSignals.clear();
+              notifyListeners();
+            }
+          }
+        } catch (e) {
+          TmsLogger().e("Error parsing match state event: $e");
         }
       }
     });

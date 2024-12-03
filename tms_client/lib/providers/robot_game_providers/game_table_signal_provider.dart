32201f43641b01c92c3c7f6467e39d2cdf2da7ba
CJBuchel <connorbuchel@gmail.com>
2024-12-03 17:23:18 +0800

    Made match controller 'clear' game table signals on load/unload

--- tms_client/lib/providers/robot_game_providers/game_table_signal_provider.dart
+++ tms_client/lib/providers/robot_game_providers/game_table_signal_provider.dart
@@ -1,11 +1,11 @@
-import 'package:echo_tree_flutter/widgets/echo_tree_provider.dart';
-import 'package:tms/generated/infra/database_schemas/game_table.dart';
+import 'package:flutter/material.dart';
+import 'package:tms/generated/infra/network_schemas/socket_protocol/match_state_event.dart';
 import 'package:tms/generated/infra/network_schemas/socket_protocol/server_socket_protocol.dart';
 import 'package:tms/generated/infra/network_schemas/socket_protocol/table_state_event.dart';
 import 'package:tms/mixins/server_event_subscriber_mixin.dart';
 import 'package:tms/utils/logger.dart';
 
-class GameTableSignalProvider extends EchoTreeProvider<String, GameTable> with ServerEventSubscribeNotifierMixin {
+class GameTableSignalProvider extends ChangeNotifier with ServerEventSubscribeNotifierMixin {
   final Map<String, String> _tableSignals = {};
 
   void _updateTableSignals(String table, String teamNumber) {
@@ -14,11 +14,7 @@ class GameTableSignalProvider extends EchoTreeProvider<String, GameTable> with S
     notifyListeners();
   }
 
-  GameTableSignalProvider()
-      : super(
-          tree: ":robot_game:tables",
-          fromJsonString: (json) => GameTable.fromJsonString(json: json),
-        ) {
+  GameTableSignalProvider() {
     subscribeToEvent(TmsServerSocketEvent.tableStateEvent, (event) {
       if (event.message != null) {
         // handle table state event
@@ -27,6 +23,24 @@ class GameTableSignalProvider extends EchoTreeProvider<String, GameTable> with S
           _updateTableSignals(tableStateEvent.table, tableStateEvent.teamNumber);
         } catch (e) {
           TmsLogger().e("Error parsing table state event: $e");
+        }
+      }
+    });
+    subscribeToEvent(TmsServerSocketEvent.matchStateEvent, (event) {
+      if (event.message != null) {
+        // handle match state event
+        try {
+          TmsServerMatchStateEvent matchStateEvent = TmsServerMatchStateEvent.fromJsonString(json: event.message!);
+          bool load = matchStateEvent.state == TmsServerMatchState.load;
+          bool unload = matchStateEvent.state == TmsServerMatchState.unload;
+
+          // clear table signals on load or unload
+          if (load || unload) {
+            _tableSignals.clear();
+            notifyListeners();
+          }
+        } catch (e) {
+          TmsLogger().e("Error parsing match state event: $e");
         }
       }
     });
